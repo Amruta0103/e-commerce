78fa2cf1c11fe60867c5e9ed0e636525dfc73765
Amruta_D <amydharap@gmail.com>
2022-08-26 11:30:12 +0530

    getting BE

--- src/server/routes/get.data.route.js
+++ src/server/routes/get.data.route.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const productRoute = express.Router();
 const { Product } = require("../models/products.model");
-
+const { productData } = require("../data");
 productRoute.route("/")
-  .get(async(req,res)=>{
+  // .get(async(req,res)=>{
+  //   try{
+  //     const products = await Product.find({})
+  //     console.log("PRODUCTS MILE", products.map((item)=>item))
+  //     res.status(200).json({ success: true});
+  //   }
+  //   catch(error){
+  //     console.log("error hai")
+  //     res.status(500).json({ success: false, error: error.message });
+  //   }
+  // })
+
+  .post(async(req,res)=>{
     try{
-      const products = await Product.find({})
-      console.log("PRODUCTS MILE", products.map((item)=>item))
-      res.status(200).json({ success: true});
+      const addData = await Product.add({productData})
+      res.json({addData});
+
+      console.log("in try block",addData)
+      res.send("in try block of products")
     }
     catch(error){
-      console.log("error hai")
-      res.status(500).json({ success: false, error: error.message });
+      res.status(500).json(error)
     }
   })
 
