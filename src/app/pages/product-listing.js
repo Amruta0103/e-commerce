047a2d447b6213cf26dee06b4576ef7de4210c77
Amruta_D <71976347+Amruta0103@users.noreply.github.com>
2022-09-11 13:57:47 +0530

    temp

--- src/app/pages/product-listing.js
+++ src/app/pages/product-listing.js
@@ -56,7 +56,7 @@ export default function ProductList() {
       .filter(({ fastDelivery }) =>
         showFastDeliveryOnly ? fastDelivery : true
       )
-      .filter(({ inStock }) => (showInventoryAll ? inStock : !inStock));
+      .filter(({ inStock }) => (showInventoryAll ? data : inStock));
   }
 
   const sortedData = getSortedData(data, sortBy);
